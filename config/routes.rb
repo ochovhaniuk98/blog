6410eeb59a95919c6fc67c26a738aa45fd2c3c27
Olha Chovhaniuk <ochovhaniuk@myseneca.ca>
2024-01-29 10:47:55 -0500

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # get "up" => "rails/health#show", as: :rails_health_check
+
+  get "blog_posts/:id", to: "blog_posts#show", as: :blog_post
+
+  # Defines the root path route ("/")
+  # controller name # action name
+  root "blog_posts#index"
+end
