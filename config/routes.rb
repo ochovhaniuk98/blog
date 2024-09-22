3fc9533f045455212ed1b9a205246ff19ea8000d
Olha Chovhaniuk <ochovhaniuk@myseneca.ca>
2024-09-22 17:36:56 -0400

    added root route and blog post controller and the view

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # controller name # function name
+  root "blog_posts#index"
 end
