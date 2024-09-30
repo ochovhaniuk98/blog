16a8936a7a3b85e405e4e52537209283644bfcc9
Olha Chovhaniuk <ochovhaniuk@myseneca.ca>
2024-09-30 09:34:17 -0400

    created a form for a new blog post

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  
   # controller name # function name
+  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
+  
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+
+  # Defines the root path route ("/")
   root "blog_posts#index"
 end
