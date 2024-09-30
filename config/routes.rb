f771454d0a3a57a8128267683c2af70c64f8ac06
Olha Chovhaniuk <ochovhaniuk@myseneca.ca>
2024-09-30 11:51:51 -0400

    user authentication functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  
+  devise_for :users
   # controller name # function name
   resources :blog_posts
   # get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
